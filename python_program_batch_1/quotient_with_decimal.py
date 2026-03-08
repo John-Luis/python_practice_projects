6e86239b4460822cffc2c029a3854b463c38f56a
John Luis <johnluisvmayor@iskolarngbayan.pup.edu.ph>
2026-03-08 09:05:19 +0800

    creation of code and testing

---
+++ python_program_batch_1/quotient_with_decimal.py
@@ -0,0 +1,10 @@
+
+def quotient():
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))
+    if num2!= 0:
+        print(num1/num2)
+    else:
+        print("Cannot divide by zero")
+
+quotient()
