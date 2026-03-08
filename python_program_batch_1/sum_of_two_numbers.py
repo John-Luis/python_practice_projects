1342682d0f3248cc917c644244f779c3794a47f3
John Luis <johnluisvmayor@iskolarngbayan.pup.edu.ph>
2026-03-08 09:00:59 +0800

    creation of code and testing

---
+++ python_program_batch_1/sum_of_two_numbers.py
@@ -0,0 +1,8 @@
+
+
+def sum_of_two_numbers():
+    num_1 = int(input("Enter first number: "))
+    num_2 = int(input("Enter second number: "))
+    print(f"{num_1} + {num_2} = {num_1 + num_2}")
+
+sum_of_two_numbers()
