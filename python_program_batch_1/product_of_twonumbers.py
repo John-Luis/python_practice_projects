86fa7833cacf208c0acf5607227fd168bdacea20
John Luis <johnluisvmayor@iskolarngbayan.pup.edu.ph>
2026-03-08 09:02:52 +0800

    creation of code and testing

---
+++ python_program_batch_1/product_of_twonumbers.py
@@ -0,0 +1,8 @@
+
+def product_of_twonumbers():
+
+    num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
+    print(num1 * num2)
+
+product_of_twonumbers()
