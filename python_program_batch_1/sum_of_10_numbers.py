b823fa5789a5d27b30af87e82786ae77c8195a93
John Luis <johnluisvmayor@iskolarngbayan.pup.edu.ph>
2026-03-08 09:08:00 +0800

    creation of code and testing

---
+++ python_program_batch_1/sum_of_10_numbers.py
@@ -0,0 +1,12 @@
+
+
+
+def sum_of_10():
+
+    total = 0
+    for i in range(10):
+        num = float(input(f"Enter number {i + 1}: "))
+        total += num
+    print(f"Total Sum: {total}")
+
+sum_of_10()
