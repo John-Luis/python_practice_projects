5e350749d95520bf4a766749ce472709402040fd
John Luis <johnluisvmayor@iskolarngbayan.pup.edu.ph>
2026-03-08 08:53:13 +0800

    Creation of code and testing

---
+++ python_program_1/numbers_equal.py
@@ -0,0 +1,7 @@
+def check_equal():
+    num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
+    if num1 == 2:
+        print("Both numbers are equal")
+    else:
+        print("Numbers are not equal")
