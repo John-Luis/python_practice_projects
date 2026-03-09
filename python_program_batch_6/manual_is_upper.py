2a048883a72d13745f0329ff6195ec83e182b7a1
John Luis <johnluisvmayor@iskolarngbayan.pup.edu.ph>
2026-03-09 16:54:35 +0800

    added the functionality where we test the letters manually, and testing... also completed

--- python_program_batch_6/manual_is_upper.py
+++ python_program_batch_6/manual_is_upper.py
@@ -1,8 +1,18 @@
 
 
-def manuel_is_upper():
+def manual_is_upper(text):
     #it will check if the string inputted is empty, if is... it will return false
     if not text:
         return False
 
-    
+    for char in text:
+
+        #here the computer is checking if the string inputted is capital or not
+        # it checks by the ASCII table.. where the a represents a number up to the z... where the number in between is all lowercase alphabet.
+        if 'a' <= char <= 'z':
+            return False
+
+    return True
+
+test_string = input("Enter a string: ")
+print(f"Is all upppercase?: {manual_is_upper(test_string)}")
