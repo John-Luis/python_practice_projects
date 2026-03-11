93f6b9c615a5118f66ec29c248a98f1c7e3b2e6b
John Luis <johnluisvmayor@iskolarngbayan.pup.edu.ph>
2026-03-12 00:04:14 +0800

    fix of function call

--- python_program_batch_1/numbers_equal.py
+++ python_program_batch_1/numbers_equal.py
@@ -1,7 +1,10 @@
 def check_equal():
+
     num1 = float(input("Enter first number: "))
     num2 = float(input("Enter second number: "))
     if num1 == 2:
         print("Both numbers are equal")
     else:
         print("Numbers are not equal")
+
+check_equal()
