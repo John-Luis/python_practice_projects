1b645c388c545c33667474c90577f046f506ce97
John Luis <johnluisvmayor@iskolarngbayan.pup.edu.ph>
2026-03-17 22:50:03 +0800

    Creation of for loop logic and testing if it works..

--- python_program_batch_7/manual_reverse_find_index.py
+++ python_program_batch_7/manual_reverse_find_index.py
@@ -5,3 +5,13 @@ text = input("Enter main string: ")
 sub = input("Enter substring to find from end: ")
 sub_len = len(sub)
 found_index = -1
+
+for i in range(len(text) - sub_len, -1, -1):
+    if text[i:i+sub_len] == sub:
+        found_index = i
+        break
+
+if found_index != -1:
+    print(f"Last index: {found_index}")
+else:
+    print("Substring not found.")
