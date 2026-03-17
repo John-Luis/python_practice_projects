151976efeaf4c5775958dedfdc2486bfe75bfad8
John Luis <johnluisvmayor@iskolarngbayan.pup.edu.ph>
2026-03-18 01:47:18 +0800

    Created a manual comparison logic to identify the most frequent number using a frequencey record

--- python_program_batch_4/most_frequent_input.py
+++ python_program_batch_4/most_frequent_input.py
@@ -9,3 +9,21 @@ while True:
         numbers.append(num)
     except ValueError:
         break
+
+# Part 2: Find the most frequent
+if numbers:
+    most_frequent = numbers[0]
+    highest_count = 0
+
+    for num in numbers:
+        # Count how many times 'num' appears in the whole list
+        current_count = numbers.count(num)
+
+        # If this number appears more than our previous record, update it
+        if current_count > highest_count:
+            highest_count = current_count
+            most_frequent = num
+
+    print(f"Number with most duplicates: {most_frequent}")
+else:
+    print("No numbers were entered.")
