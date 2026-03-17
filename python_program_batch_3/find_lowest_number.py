bf4353bae5ab75c401725955046dbe4cf31c4aa6
John Luis <johnluisvmayor@iskolarngbayan.pup.edu.ph>
2026-03-18 02:03:58 +0800

    created the data colletion loop for numerical values with also errorhandling

---
+++ python_program_batch_3/find_lowest_number.py
@@ -0,0 +1,9 @@
+numbers = []
+print("Enter numbers (input any letter to stop):")
+
+while True:
+    try:
+        num = float(input("> "))
+        numbers.append(num)
+    except ValueError:
+        break
