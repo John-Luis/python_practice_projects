dbe9d572cba173bd1aab135718343b13e95f8325
John Luis <johnluisvmayor@iskolarngbayan.pup.edu.ph>
2026-03-18 01:44:33 +0800

    Added a input loop with error handling for collecting user numbers, and a conditiona logic to decide and print the highest values from the collected numerical list.

--- python_program_batch_4/find_highest_number.py
+++ python_program_batch_4/find_highest_number.py
@@ -1,3 +1,13 @@
 
 numbers = []
 print("Enter numbers (input any non-number to stop):")
+
+while True:
+    try:
+        num = float(input("> "))
+        numbers.append(num)
+    except ValueError:
+        break
+
+if numbers:
+    print(f"Highest number: {max(numbers)}")
