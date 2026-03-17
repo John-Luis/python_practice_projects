3229ea0fc0b9adb0a49a6a8c4a6472d06ccfc075
John Luis <johnluisvmayor@iskolarngbayan.pup.edu.ph>
2026-03-17 22:57:43 +0800

    Creation of logic and testing... succesful, so its the final code

--- python_program_batch_7/manual_zero_filling.py
+++ python_program_batch_7/manual_zero_filling.py
@@ -2,3 +2,10 @@
 
 text = input("Enter number/string: ")
 width = int(input("Enter total width: "))
+
+zeros = width - len(text)
+result = ("0" * zeros + text) \
+    if zeros > 0 \
+    else text
+
+print(f"Result: {result}")
