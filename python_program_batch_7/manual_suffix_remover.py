519a4a87687ce7c15feec4658479d503585e97eb
John Luis <johnluisvmayor@iskolarngbayan.pup.edu.ph>
2026-03-17 23:06:46 +0800

    creation of def function logic that functions as manual suffix remover...

--- python_program_batch_7/manual_suffix_remover.py
+++ python_program_batch_7/manual_suffix_remover.py
@@ -3,3 +3,13 @@
 text = input("Enter main string: ")
 suffix = input("Enter suffix to remove: ")
 
+# Check if the text ends with the suffix
+def manual_suffix_remover(text):
+    if suffix and text.endswith(suffix):
+        result = text[:-len(suffix)]
+    else:
+        result = text
+
+    print(f"Result: {result}")
+    
+manual_suffix_remover(text)
