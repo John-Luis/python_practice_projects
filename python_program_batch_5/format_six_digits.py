da70ff37b9389fa06616f72b3c5df0bbcad275da
John Luis <johnluisvmayor@iskolarngbayan.pup.edu.ph>
2026-03-18 01:11:10 +0800

    Applying the .zfill method built in function in python. Ensuring the string is atleast 6 characters long by prepending zeros if the input is shorter

--- python_program_batch_5/format_six_digits.py
+++ python_program_batch_5/format_six_digits.py
@@ -1,3 +1,8 @@
 
 number = input("Input a number (0-1000): ")
 
+def format_six_number(number):
+    print(f"Before: {number:>6}")
+    print(f"Output: {number.zfill(6)}")
+
+format_six_number(number)
