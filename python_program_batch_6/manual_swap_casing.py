3f906c769effb1e006cd5219651a7b08e32d54e6
John Luis <johnluisvmayor@iskolarngbayan.pup.edu.ph>
2026-03-18 00:43:21 +0800

    Implement swapcase using dual ASCII range checks. Like the previous examples where we use ASCII values.

--- python_program_batch_6/manual_swap_casing.py
+++ python_program_batch_6/manual_swap_casing.py
@@ -1,2 +1,12 @@
 text = input("Enter string: ")
 result = ""
+
+for char in text:
+    if 'A' <= char <= 'Z':
+        result += chr(ord(char) + 32)
+    elif 'a' <= char <= 'z':
+        result += chr(ord(char) - 32)
+    else:
+        result += char
+
+print(f"Result: {result}")
