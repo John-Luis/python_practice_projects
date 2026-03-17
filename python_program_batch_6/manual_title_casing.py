8e739be1428a8a1e0848aae8b18ee4d194bfdfb1
John Luis <johnluisvmayor@iskolarngbayan.pup.edu.ph>
2026-03-18 01:07:02 +0800

    Use a loop and a boolean flag to track word starts and manually shift character ASCII values for title casing

--- python_program_batch_6/manual_title_casing.py
+++ python_program_batch_6/manual_title_casing.py
@@ -1,3 +1,21 @@
 text = input("Enter string: ")
 result = ""
 new_word = True
+
+for char in text:
+    if char == " ":
+        result += " "
+        new_word = True
+    elif new_word:
+        if 'a' <= char <= 'z':
+            result += chr(ord(char) - 32)
+        else:
+            result += char
+        new_word = False
+    else:
+        if 'A' <= char <= 'Z':
+            result += chr(ord(char) + 32)
+        else:
+            result += char
+
+print(f"Result: {result}")
