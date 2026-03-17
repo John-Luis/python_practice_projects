b80b08cacfaff812ce29d61e94597071c436a690
John Luis <johnluisvmayor@iskolarngbayan.pup.edu.ph>
2026-03-18 00:04:35 +0800

    it breaks the loop the very first time a match is found to return the earliest index

--- python_program_batch_7/manual_find_index.py
+++ python_program_batch_7/manual_find_index.py
@@ -3,3 +3,10 @@
 text = input("Enter main string: ")
 sub = input("Enter substring to find: ")
 found_index = -1
+
+for i in range(len(text) - len(sub) + 1):
+    if text[i:i+len(sub)] == sub:
+        found_index = i
+        break
+
+print(f"First index: {found_index}")
