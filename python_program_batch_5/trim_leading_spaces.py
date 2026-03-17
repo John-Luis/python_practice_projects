aeddaaff3530bb052bf6a83940b29d76a5c369ff
John Luis <johnluisvmayor@iskolarngbayan.pup.edu.ph>
2026-03-18 01:04:32 +0800

    creation of a def function, where the parameter is named text.  The function works by a built in python function lstrip that automatically removes the leading spaces

--- python_program_batch_5/trim_leading_spaces.py
+++ python_program_batch_5/trim_leading_spaces.py
@@ -1,3 +1,10 @@
 
 
 fullname = input("Input Name with leading spaces: ")
+
+def trim_leading_spaces(text):
+    print(f"Before: {text}")
+    print(f"Output: {text.lstrip()}")
+
+trim_leading_spaces(fullname)
+
