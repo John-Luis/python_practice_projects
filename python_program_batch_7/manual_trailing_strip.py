0113fba7e22652b0fa7fb70ecbb51e3f9bf81918
John Luis <johnluisvmayor@iskolarngbayan.pup.edu.ph>
2026-03-17 22:35:52 +0800

    creation of logic, for loop... displaying and testing

--- python_program_batch_7/manual_trailing_strip.py
+++ python_program_batch_7/manual_trailing_strip.py
@@ -6,4 +6,14 @@ text = input("Enter a string with spaces at the end: ")
 end_index = len(text)
 
 def manual_trailing_strip(text):
-    
+    for i in range(len(text) - 1, -1, -1):
+        if text[i] != " ":
+            end_index = i + 1
+            break
+
+    print(f"Before:{text}\nAfter:{text[:end_index]}")
+
+
+
+manual_trailing_strip(text)
+
