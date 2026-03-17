ad45a3a82f221c499b8b397b1651a956e0512b2f
John Luis <johnluisvmayor@iskolarngbayan.pup.edu.ph>
2026-03-18 01:34:17 +0800

    creating a input loop to get the numerical values into the list, until a non-number or integer is entered.

---
+++ python_program_batch_4/sort_descending_inputs.py
@@ -0,0 +1,9 @@
+numbers = []
+print("Enter numbers (input any non-number to stop):")
+
+while True:
+    try:
+        num = float(input("> "))
+        numbers.append(num)
+    except ValueError:
+        break
