dc8ea5695654875d9ab0f2c3369caec44029c36d
John Luis <johnluisvmayor@iskolarngbayan.pup.edu.ph>
2026-03-18 00:30:24 +0800

    we take a slice from the end of the string equal to the length of the suffix text[-len(suffix):] and compare it

--- python_program_batch_6/manual_suffix_check.py
+++ python_program_batch_6/manual_suffix_check.py
@@ -1,2 +1,12 @@
 text = input("Enter string: ")
 suffix = input  ("Enter suffix to check: ")
+
+def manual_suffix_check(text,suffix):
+    if len(suffix) == 0:
+        match = True
+    else:
+        match = text[-len(suffix):] == suffix
+
+    print(f"Ends with '{suffix}': {match}")
+
+manual_suffix_check(text,suffix)
