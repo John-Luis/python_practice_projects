3ba4e91c330c55a1225121b4a21f9074c44e32e4
John Luis <johnluisvmayor@iskolarngbayan.pup.edu.ph>
2026-03-18 02:37:33 +0800

    Created the logic: By asking and comparing them by simple if and else statements

---
+++ python_program_batch_1/bigger_number.py
@@ -0,0 +1,9 @@
+
+
+ask_1 = float(input("Enter first number: "))
+ask_2 = float(input("Enter second number: "))
+
+if ask_1 > ask_2:
+    print(f"{ask_1} is greater than {ask_2}")
+else:
+    print(f"{ask_2} is greater than {ask_1}")
