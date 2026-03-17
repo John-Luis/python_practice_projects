9678bba34f98aee59ea0a372d1796c21a5acbb8a
John Luis <johnluisvmayor@iskolarngbayan.pup.edu.ph>
2026-03-18 00:08:54 +0800

    Implement manual rstrip logic using backward iteration

--- python_program_batch_6/manual_leading_strip.py
+++ python_program_batch_6/manual_leading_strip.py
@@ -2,3 +2,12 @@
 
 text = input("Enter string with leading spaces: ")
 start_index = 0
+
+for i in range(len(text)):
+    if text[i] != " ":
+        start_index = i
+        break
+else:
+    start_index = len(text) # Handles string with only spaces
+
+print(f"Result: '{text[start_index:]}'")
