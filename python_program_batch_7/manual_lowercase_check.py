b853c55cd82157e06ca5d85596fd6ce1e5ae9870
John Luis <johnluisvmayor@iskolarngbayan.pup.edu.ph>
2026-03-17 23:19:59 +0800

    Creation of logic: where it assumes the string is lowercase untit finds if the character is capital by A through Z range in the ascii table values

--- python_program_batch_7/manual_lowercase_check.py
+++ python_program_batch_7/manual_lowercase_check.py
@@ -3,3 +3,13 @@
 text = input("Enter string: ")
 is_lower = True
 has_alpha = False
+
+for char in text:
+    if 'A' <= char <= 'Z':
+        is_lower = False
+        break
+    # this is where we use again the ascii value, technically the values in it.
+    if 'a' <= char <= 'z':
+        has_alpha = True
+
+print(f"Result: {is_lower and has_alpha}")
