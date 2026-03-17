652944cb9c72861178b9b95c1b06e553e49c587b
John Luis <johnluisvmayor@iskolarngbayan.pup.edu.ph>
2026-03-18 02:30:07 +0800

    added a extra explanation in comment

--- python_program_batch_2/count_even_numbers_in_10.py
+++ python_program_batch_2/count_even_numbers_in_10.py
@@ -1,5 +1,6 @@
 even_count = 0
 
+# We use a counter variable initialized at 0. For each of the 10 inputs, we check if num % 2 == 0. If true, we increment the counter.
 for i in range(10):
     num = int(input(f"Enter number {i+1}: "))
     if num % 2 == 0:
