c757ccc2faaa1c30c3df3d3017e7440182540fc1
John Luis <johnluisvmayor@iskolarngbayan.pup.edu.ph>
2026-03-17 23:12:11 +0800

    creation of logic, testing, and see if any bugs

--- python_program_batch_7/manual_uppercase_convert.py
+++ python_program_batch_7/manual_uppercase_convert.py
@@ -2,3 +2,12 @@
 
 text = input("Enter string: ")
 result = ""
+
+for char in text:
+    #using the ascii value... the lowercase letter were shifted to uppercase version by ascii value being 32 higher.
+    if 'a' <= char <= 'z':
+        result += chr(ord(char) - 32)
+    else:
+        result += char
+
+print(f"Result: {result}")
