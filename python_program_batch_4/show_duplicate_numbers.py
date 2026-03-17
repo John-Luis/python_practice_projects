0a9eb7f8d6440b5e31eefebd6e1973239cd1a514
John Luis <johnluisvmayor@iskolarngbayan.pup.edu.ph>
2026-03-18 01:36:48 +0800

    by creating a logic to check the duplicate values by tracking the occurences and results. It will then display the list of the duplicate numbers in the list.

--- python_program_batch_4/show_duplicate_numbers.py
+++ python_program_batch_4/show_duplicate_numbers.py
@@ -2,3 +2,10 @@ numbers = []
 for i in range(10):
     num = float(input(f"Enter number {i+1}: "))
     numbers.append(num)
+
+duplicates = []
+for num in numbers:
+    if numbers.count(num) > 1 and num not in duplicates:
+        duplicates.append(num)
+
+print(f"Numbers with duplicates: {duplicates}")
