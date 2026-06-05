35a126f3d146d354e8f3cca7977e4d29bac555db
John Luis Mayor <johnluisvmayor@iskolarngbayan.pup.edu.ph>
2026-06-06 05:58:15 +0800

    Update manual_reverse_find_index.py
    short version code

--- python_program_batch_7/manual_reverse_find_index.py
+++ python_program_batch_7/manual_reverse_find_index.py
@@ -1,17 +1,9 @@
 
 
 
-text = input("Enter main string: ")
-sub = input("Enter substring to find from end: ")
-sub_len = len(sub)
-found_index = -1
+text = input("enter string")
+sub = input("enter substring")
 
-for i in range(len(text) - sub_len, -1, -1):
-    if text[i:i+sub_len] == sub:
-        found_index = i
-        break
-
-if found_index != -1:
-    print(f"Last index: {found_index}")
-else:
-    print("Substring not found.")
+for i in range (len(text) - len(sub), -1, -1):
+    if text[i:i+len(sub)] == sub:
+        print(i)
