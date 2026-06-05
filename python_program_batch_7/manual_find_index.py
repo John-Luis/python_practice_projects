d94ee59f5a89d0fe43a408c21266408b15e598c5
John Luis Mayor <johnluisvmayor@iskolarngbayan.pup.edu.ph>
2026-06-06 05:52:12 +0800

    Update manual_find_index.py

--- python_program_batch_7/manual_find_index.py
+++ python_program_batch_7/manual_find_index.py
@@ -1,12 +1,9 @@
 
 
-text = input("Enter main string: ")
-sub = input("Enter substring to find: ")
-found_index = -1
+text = input("enter text")
+sub = input("enter string")
 
-for i in range(len(text) - len(sub) + 1):
+for i in range(len(text)-len(sub)+1):
     if text[i:i+len(sub)] == sub:
-        found_index = i
-        break
-
-print(f"First index: {found_index}")
+        print(i)
+        
