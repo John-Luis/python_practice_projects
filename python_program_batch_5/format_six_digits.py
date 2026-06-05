7a79d0c2309268124d801ae9381aeafa81527dbd
John Luis Mayor <johnluisvmayor@iskolarngbayan.pup.edu.ph>
2026-06-06 06:59:51 +0800

    Update format_six_digits.py

--- python_program_batch_5/format_six_digits.py
+++ python_program_batch_5/format_six_digits.py
@@ -1,8 +1,3 @@
 
 number = input("Input a number (0-1000): ")
-
-def format_six_number(number):
-    print(f"Before: {number:>6}")
-    print(f"Output: {number.zfill(6)}")
-
-format_six_number(number)
+print(f"{number:06d}")
