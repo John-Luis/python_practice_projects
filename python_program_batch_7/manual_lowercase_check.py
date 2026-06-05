542dc50fd26bdbdc58bad9080c9b3849397c019f
John Luis Mayor <johnluisvmayor@iskolarngbayan.pup.edu.ph>
2026-06-06 05:55:53 +0800

    Update manual_lowercase_check.py
    updated shorted code

--- python_program_batch_7/manual_lowercase_check.py
+++ python_program_batch_7/manual_lowercase_check.py
@@ -1,15 +1,13 @@
 
 
-text = input("Enter string: ")
+text = input("enter text")
 is_lower = True
-has_alpha = False
+has_word = False
 
 for char in text:
-    if 'A' <= char <= 'Z':
+    if "a" <= char <= "z":
+        has_word = True
+    elif "A"<= char <= "Z":
         is_lower = False
-        break
-    # this is where we use again the ascii value, technically the values in it.
-    if 'a' <= char <= 'z':
-        has_alpha = True
-
-print(f"Result: {is_lower and has_alpha}")
+        
+print(is_lower and has_word)
