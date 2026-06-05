77c22061ecc07003512a97551cddc39000ddd106
John Luis Mayor <johnluisvmayor@iskolarngbayan.pup.edu.ph>
2026-06-06 06:05:24 +0800

    Update display_first_entries.py
    short version of the bbc kde

--- python_program_batch_3/display_first_entries.py
+++ python_program_batch_3/display_first_entries.py
@@ -1,13 +1,6 @@
+ num = []
 
-numbers = []
-for i in range(10):
-    num = float(input(f"Enter number {i+1}: "))
-    numbers.append(num)
-
-seen = []
-for num in numbers:
-    if num not in seen:
-        seen.append(num)
-
-print("Unique list (first entries only):")
-print(seen)
+for i in range (10):
+    num.append(float(input("enter number")))
+    
+print(set(num))
