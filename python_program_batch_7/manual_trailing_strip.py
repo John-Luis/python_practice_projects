d0742e1239e8978f76fdde8c02007c03017793a0
John Luis Mayor <johnluisvmayor@iskolarngbayan.pup.edu.ph>
2026-06-06 06:02:50 +0800

    Update manual_trailing_strip.py
    updated short code

--- python_program_batch_7/manual_trailing_strip.py
+++ python_program_batch_7/manual_trailing_strip.py
@@ -1,29 +1,11 @@
 
+text = input("enter text")
+count = 0
 
+for i in range (len(text)-1,-1,-1):
+    if text[i] != " ":
+        count = 1 + i
+        break
 
-
-text = input("Enter a string with spaces at the end: ")
-end_index = len(text)
-
-def manual_trailing_strip(text):
-    for i in range(len(text) - 1, -1, -1):
-        if text[i] != " ":
-            end_index = i + 1
-            break
-    else:
-        # If the string was only spaces
-        end_index = 0
-
-    # A logic to show spaces as underscore for exhibition
-    # replacing the spaces with underscore '_', only for visuals.
-
-    visible_before = text.replace(" ", "_")
-    visible_after = text[:end_index].replace(" ", "_")
-
-    print("underscore for visual difference")
-    print(f"Before: {visible_before}")
-    print(f"After:  {visible_after}")
-
-
-manual_trailing_strip(text)
-
+print(text[:count])
+    
